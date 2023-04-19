1357ade896e56ccbb2187d4765d4c217ae375962
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-19 16:36:25 +0200

    HttpEventType

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,6 +3,7 @@ import {Room, RoomsList} from "./rooms";
 import {HeaderComponent} from "../header/header.component";
 import {RoomsService} from "./services/rooms.service";
 import {Observable} from "rxjs";
+import {HttpEventType} from "@angular/common/http";
 
 @Component({
   selector: 'app-rooms',
@@ -22,6 +23,7 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
   roomLists: RoomsList[] = [];
   selectedRoom!: RoomsList;
   title: string = '';
+  totalBytes = 0;
 
   stream = new Observable(observer => {
     observer.next('user1');
@@ -43,6 +45,27 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
     this.roomService.getRooms().subscribe(rooms => {
       this.roomLists = rooms;
     });
+
+    this.roomService.getPhotos().subscribe((event) => {
+      switch (event.type) {
+        case HttpEventType.Sent: {
+          console.log('request has been made');
+          break;
+        }
+        case HttpEventType.ResponseHeader:{
+          console.log('request success');
+          break;
+        }
+        case HttpEventType.DownloadProgress:{
+          this.totalBytes += event.loaded;
+          break;
+        }
+        case HttpEventType.Response:{
+          console.log(event.body);
+          break;
+        }
+      }
+    });
   }
 
   selectRoom(room: RoomsList) {
@@ -61,7 +84,32 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
       photos: 'https://thumbs.dreamstime.com/b/hotel-room-beautiful-orange-sofa-included-43642330.jpg'
     }
     // this.roomlist.push(room);
-    this.roomLists = [...this.roomLists, room];
+    // this.roomLists = [...this.roomLists, room];
+    this.roomService.addRoom(room).subscribe((data) => {
+      this.roomLists = data
+    });
+  }
+
+  updateRoom() {
+    const room: RoomsList = {
+      roomNumber: '3',
+      rating: 4.3,
+      roomType: '3',
+      amenities: 'Air conditioner, Free Wi-Fi',
+      price: 1111,
+      checkinTime: new Date('11-nov-2021'),
+      checkoutTime: new Date('20-nov-2021'),
+      photos: 'https://thumbs.dreamstime.com/b/hotel-room-beautiful-orange-sofa-included-43642330.jpg'
+    }
+    this.roomService.updateRoom(room).subscribe((data) => {
+      this.roomLists = data
+    });
+  }
+
+  deleteRoom() {
+    this.roomService.deleteRoom('3').subscribe((data) => {
+      this.roomLists = data
+    });
   }
 
   ngDoCheck(): void {
--- src/app/rooms/services/rooms.service.ts
+++ src/app/rooms/services/rooms.service.ts
@@ -3,7 +3,7 @@ import {RoomsList} from "../rooms";
 import {environment} from "../../../enviroments/environments";
 import {APP_SERVICE_CONFIG} from "../../AppConfig/app.service";
 import {AppConfig} from "../../AppConfig/appconfig.interface";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpRequest} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +18,28 @@ export class RoomsService {
 
   getRooms() {
     return this.http.get<RoomsList[]>('/api/rooms');
+  }
+
+  addRoom(room: RoomsList){
+    return this.http.post<RoomsList[]>('api/rooms', room);
+  }
+
+  updateRoom(room: RoomsList){
+    return this.http.put<RoomsList[]>(`api/rooms/${room.roomNumber}`, room);
+  }
+
+  deleteRoom(id: string){
+    return this.http.delete<RoomsList[]>(`api/rooms/${id}`);
+  }
+
+  getPhotos(){
+    const request = new HttpRequest(
+      'GET',
+      `https://jsonplaceholder.typicode.com/photos`,
+      {
+        reportProgress: true
+      }
+    );
+    return this.http.request(request);
   }
 }
