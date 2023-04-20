df500cf5612e9003b5651e99b0d6052502c36d5a
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-20 12:01:26 +0200

    template forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {RoomsComponent} from "./rooms/rooms.component";
 import {ContainerComponent} from "./container/container.component";
 import {NotfoundComponent} from "./notfound/notfound.component";
 import {RoomsBookingComponent} from "./rooms/rooms-booking/rooms-booking.component";
+import {RoomsAddComponent} from "./rooms/rooms-add/rooms-add.component";
 
 const routes: Routes = [
   {path: 'employee', component: EmployeeComponent},
   {path: 'rooms', component: RoomsComponent},
+  {path: 'rooms/add', component:RoomsAddComponent},
   {path: 'rooms/:id', component:RoomsBookingComponent},
   {path: '', redirectTo: '/rooms', pathMatch: 'full'},
   {path: '**', component:NotfoundComponent}
